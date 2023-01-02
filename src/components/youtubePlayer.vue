1e8070ead5f00c25e0f156996f5256a009d8025b
bradlee <bradlee@starfruitsolutions.com>
2023-01-02 17:24:43 -0500

    Add separate youtube video component

---
+++ src/components/youtubePlayer.vue
@@ -0,0 +1,31 @@
+<template>
+    <iframe
+        :title="name"
+        :src="source"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen=""
+    />    
+</template>
+
+<script>
+
+export default{
+    props:['name', 'url'],
+    computed:{
+        source(){
+            const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
+            const match = this.url.match(regExp)
+
+            return '//www.youtube.com/embed/' + match[2]
+        }
+    }    
+}
+</script>
+
+<style lang="scss" scoped>
+iframe {
+  width: 100%;
+  aspect-ratio: 16 / 9;
+}
+</style>
