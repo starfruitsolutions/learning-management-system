d76887eb4a7c1f6462bc219e8675a1f46af05b1f
bradlee <bradlee@starfruitsolutions.com>
2023-01-02 17:23:33 -0500

    adds markdown component

---
+++ src/components/markdown.vue
@@ -0,0 +1,24 @@
+<template>
+    <markdown :source="source" html break/>
+</template>
+
+<script>
+/*
+uses markdown-it package. 
+We'll want to ensure sanitization or make our own parser if we allow others to make courses
+*/
+import markdown from 'vue3-markdown-it'
+import 'highlight.js/styles/monokai.css'
+
+export default{
+    props:['source'],
+    components: {
+        markdown
+    },
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
