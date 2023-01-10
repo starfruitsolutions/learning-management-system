69e4f95799a2041835d9a3360842b5c8f4727008
bradlee <bradlee@starfruitsolutions.com>
2023-01-10 03:09:15 -0500

    add reusable card components

---
+++ src/components/cards/course.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-card class="fill-height">
+    <v-img
+      height="220"
+      :src="asset('courses', id, image)"
+      gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,0), rgba(53, 11, 92, 1)"
+      cover
+      class="align-end"
+    >
+    </v-img>
+    <v-card-item height="600">
+      <v-progress-linear
+        :model-value="progress"
+        color="light-blue"
+        height="10"
+        striped
+        class="my-5"
+      />
+      <v-card-title class="text-h5">{{ name }}</v-card-title>
+      <v-chip
+        v-for="category in categories"
+        :key="category"
+        color="primary"
+        label
+        variant="outlined"
+        class="mr-4 my-2"
+        size="small"
+      >
+        {{ category }}
+      </v-chip>
+      <p class="my-3">{{ description }}</p>
+    </v-card-item>
+
+    <v-card-actions>
+      <v-btn :to="{ path: '/course/' + id }" block> {{ linkText }} </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { asset } from '@/stores/pocketbase'
+
+export default {
+  props: [
+    'id',
+    'name',
+    'categories',
+    'image',
+    'currentUnit',
+    'progress',
+    'description',
+    'linkText',
+  ],
+  methods: {
+    asset,
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/cards/list.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card>
+    <v-img
+      height="50"
+      src="https://img.freepik.com/premium-vector/dark-purple-abstract-background-that-represents-speed-is-decorated-with-dots-lines_49459-670.jpg?w=2000"
+      gradient="to bottom right, rgba(97, 29, 191,.7), rgba(67, 9, 117,.7)"
+      cover
+    >
+      <v-card-title class="text-center">{{ header }}</v-card-title>
+    </v-img>
+    <v-card-item>
+      <v-list lines="two">
+        <v-list-item
+          v-for="item in items"
+          :key="item.id"
+          :title="item.title"
+          :prepend-avatar="avatarSource(item.id, avatarType)"
+        >
+          <template v-slot:subtitle>
+            <div>{{ item.subtitle }}</div>
+          </template>
+          <template v-slot:append>
+            <v-btn
+              color="deep-purple"
+              icon="fa-solid fa-eye"
+              size="x-small"
+            ></v-btn>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-card-item>
+    <v-card-actions>
+      <v-btn block>See More</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { avatarSource } from '@/utilities/dicebear'
+
+export default {
+  props: ['header', 'items', 'moreUrl', 'avatarType', 'linkPrefix'],
+  methods: {
+    avatarSource,
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
